6ecb6f585e159558faf1e28dd07537b32b2be2bb
weizheng92 <1029660645@qq.com>
2015-03-16 21:08:16 +0800

    4th version

--- test.c
+++ test.c
@@ -6,10 +6,18 @@
  ************************************************************************/
 
 #include<stdio.h>
-
+int gcd(int a,int b)
+{
+	if(b==0) return a;
+	else return gcd(b,a%b);
+}
 int main()
 {
-	int index;
-	printf("hello world\n")
+    int a,b;
+	while(scanf("%d%d",&a,&b)!=EOF)
+	{
+		printf("%d\n",gcd(a,b));
+	}
+
 	return 0;
 }
