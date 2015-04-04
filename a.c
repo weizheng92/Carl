85432572656cedfae2002607ba29f14d7696a8a4
weizheng92 <1029660645@qq.com>
2015-04-04 16:03:52 +0800

    test

---
+++ a.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: a.c
+	> Author: WeiZheng
+	> Mail:1029660645@qq.com 
+	> Created Time: Sat 04 Apr 2015 03:57:27 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+	printf("hellow world\n");
+	return 0;
+}
